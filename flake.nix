986059e7b8fcd73ea9fdddc5e37f9f43797d9d5b
dearfl <i@flr.me>
2025-03-01 04:23:48 +0800

    add base home manager configs

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, home-manager, ... }:
     {
       nixosConfigurations = {
         # my main laptop x1 carbon 8th gen
@@ -14,6 +18,14 @@
           system = "x86_64-linux";
           modules = [
             ./hosts/x1c
+
+            # home manager
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.flr = import ./home/flr.nix;
+            }
           ];
         };
       };
