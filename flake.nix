602e81a6fe42ccd46470c62ce5922f2b3f11197b
dearfl <i@flr.me>
2025-03-01 04:33:49 +0800

    add normal user git config

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.flr = import ./home/flr.nix;
+              home-manager.users.flr = import ./home/users/flr.nix;
             }
           ];
         };
---
+++ home/users/flr.nix
@@ -0,0 +1,17 @@
+_: {
+  imports = [
+    ../modules/git.nix
+  ];
+
+  config = {
+    home = {
+      username = "flr";
+      homeDirectory = "/home/flr";
+      stateVersion = "24.11";
+      sessionVariables.NIXOS_OZONE_WL = "1";
+    };
+
+    # let home manager manage itself
+    programs.home-manager.enable = true;
+  };
+}
