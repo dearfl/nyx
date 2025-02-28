95b254619049755657a5611ef0f68ab67b2d0d7c
dearfl <i@flr.me>
2025-03-01 03:52:17 +0800

    refact doas

--- modules/doas.nix
+++ modules/doas.nix
@@ -1,15 +1,18 @@
 { config, ... }:
+let
+  mkRule = user: {
+    users = [ user ];
+    keepEnv = true;
+    persist = true;
+    setEnv = [ "SUDO_UID=${toString config.users.users.${user}.uid}" ];
+  };
+in
 {
   config = {
     security.sudo.enable = false;
     security.doas = {
       enable = true;
-      extraRules = map (user: {
-        users = [ user ];
-        keepEnv = true;
-        persist = true;
-        setEnv = [ "SUDO_UID=${toString config.users.users.${user}.uid}" ];
-      }) [ "flr" ];
+      extraRules = map mkRule [ "flr" ];
     };
   };
 }
