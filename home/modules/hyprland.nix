15e69d511211733e634d9fbaf2481fc7098c7a65
dearfl <i@flr.me>
2025-03-01 18:01:32 +0800

    refact hyprland workspace rule generation

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -57,18 +57,18 @@
               ++ (
                 # workspaces
                 # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-                builtins.concatLists (
-                  builtins.genList (
-                    i:
+                let
+                  mkWorkspaceRule =
+                    idx:
                     let
-                      ws = i + 1;
+                      ws = toString (idx + 1);
                     in
                     [
-                      "$mod, code:1${toString i}, workspace, ${toString ws}"
-                      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-                    ]
-                  ) 9
-                )
+                      "$mod, ${ws}, workspace, ${ws}"
+                      "$mod SHIFT, ${ws}, movetoworkspace, ${ws}"
+                    ];
+                in
+                builtins.concatLists (builtins.genList mkWorkspaceRule 9)
               );
           };
         };
