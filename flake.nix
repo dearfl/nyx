91ef74db61d3399e66baa70813ac6d0df8717979
dearfl <i@flr.me>
2025-03-03 22:30:29 +0800

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -10,40 +10,63 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
-    {
-      nixosConfigurations = {
-        # my main laptop x1 carbon 8th gen
-        x1c = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/x1c
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { withSystem, ... }:
+      {
+        flake = {
+          nixosConfigurations = {
+            # my main laptop x1 carbon 8th gen
+            x1c = withSystem "x86_64-linux" ({ system, ... }:
+              with inputs.nixpkgs;
+              lib.nixosSystem {
+                inherit system;
 
-            # home manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.flr = import ./home/users/flr.nix;
-            }
-          ];
+                modules = [
+                  ./hosts/x1c
+
+                  # home manager
+                  home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.flr = import ./home/users/flr.nix;
+                  }
+                ];
+              }
+            );
+
+            # my old laptop
+            old = withSystem "x86_64-linux" ({ system, ... }:
+              with inputs.nixpkgs;
+              lib.nixosSystem {
+                inherit system;
+
+                modules = [
+                  ./hosts/old
+
+                  # home manager
+                  home-manager.nixosModules.home-manager
+                  {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.flr = import ./home/users/flr.nix;
+                  }
+                ];
+              }
+            );
+          };
         };
 
-        # my old laptop
-        old = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/old
-
-            # home manager
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.flr = import ./home/users/flr.nix;
-            }
-          ];
-        };
-      };
-    };
+        systems = [
+          "x86_64-linux"
+        ];
+      }
+    );
 }
