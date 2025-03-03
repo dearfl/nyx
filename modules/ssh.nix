b5f96f7a0e01bc7de4a9ab3b7327f40b1bc27c02
dearfl <i@flr.me>
2025-03-03 22:28:50 +0800

    define host key in openssh

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,6 +7,13 @@ _: {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
       };
+      # tmp fix for vaultix
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
     };
   };
 }
