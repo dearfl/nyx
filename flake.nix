e8c7e676cefe56e1e074f789d67af6c1835c707b
dearfl <i@flr.me>
2025-03-13 12:36:11 +0800

    add flake-parts inputs

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
