4c4fcb84b9c9165471f6ae6a588cf7802ddde046
dearfl <i@flr.me>
2025-03-18 15:54:41 +0800

    add fontconfig

---
+++ home/modules/fontconfig.nix
@@ -0,0 +1,35 @@
+_: {
+  config = {
+    fonts.fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+          "Noto Serif CJK SC"
+          "Noto Serif CJK TC"
+          "Noto Serif CJK JP"
+          "Noto Serif CJK KR"
+        ];
+        sansSerif = [
+          "Noto Sans"
+          "Noto Sans CJK SC"
+          "Noto Sans CJK TC"
+          "Noto Sans CJK JP"
+          "Noto Sans CJK KR"
+        ];
+        monospace = [
+          "Noto Mono"
+          "Sarasa Term SC"
+          "Sarasa Term TC"
+          "Sarasa Term J"
+        ];
+        emoji = [
+          "Noto Color Emoji"
+          "Noto Emoji"
+        ];
+      };
+    };
+  };
+}
+
+
