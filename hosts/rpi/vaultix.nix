1ed62c15f8da5431975fc0ab0fbe2c5b032f9dd3
dearfl <i@flr.me>
2025-03-24 09:37:30 +0800

    update rpi host key after reinstall

--- hosts/rpi/vaultix.nix
+++ hosts/rpi/vaultix.nix
@@ -2,7 +2,7 @@ _: {
   config = {
     vaultix = {
       settings = {
-        hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILvbvKh4oC436cLDoRvZKxD7yBR9s985xpLoPwnZk1bE";
+        hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyQdgMFv/ExMUfYmn6uh69EUtX2aOO6TdZOh/DDFXZX";
       };
     };
   };
