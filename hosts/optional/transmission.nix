fe35565cccaed401b9a2a8c0bd0fb602faddc901
dearfl <i@flr.me>
2025-04-07 23:56:05 +0800

    update transmission user/group

--- hosts/optional/transmission.nix
+++ hosts/optional/transmission.nix
@@ -2,6 +2,8 @@ _: {
   config = {
     services.transmission = {
       enable = true;
+      user = "flr";
+      group = "users";
       home = "/data/transmission";
       settings = {
         watch-dir-enabled = true;
