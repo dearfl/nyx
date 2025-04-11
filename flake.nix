efd893d3f448086e5eaf328547d09412581927cf
dearfl <i@flr.me>
2025-04-12 00:57:44 +0800

    add bevy template

--- flake.nix
+++ flake.nix
@@ -52,6 +52,10 @@
               path = ./templates/rust;
               description = "Rust template, using Naersk";
             };
+            bevy = {
+              path = ./templates/bevy;
+              description = "Bevy template";
+            };
           };
 
           nixosConfigurations = {
