c71d6831e96e11811a80527230f3981d7f81b2fc
dearfl <i@flr.me>
2025-04-24 01:31:09 +0800

    enable fcitx5 for wayland

--- home/modules/fcitx5.nix
+++ home/modules/fcitx5.nix
@@ -4,13 +4,16 @@
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
-      fcitx5.addons = [
-        pkgs.fcitx5-gtk
-        pkgs.fcitx5-chinese-addons
-        pkgs.fcitx5-pinyin-zhwiki
-        pkgs.fcitx5-tokyonight
-        pkgs.libsForQt5.fcitx5-qt
-      ];
+      fcitx5 = {
+        addons = [
+          pkgs.fcitx5-gtk
+          pkgs.fcitx5-chinese-addons
+          pkgs.fcitx5-pinyin-zhwiki
+          pkgs.fcitx5-tokyonight
+          pkgs.libsForQt5.fcitx5-qt
+        ];
+        waylandFrontend = true;
+      };
     };
   };
 }
