ee2e53570fbfb192a8761efda39f350a0082953f
dearfl <i@flr.me>
2025-05-09 05:09:56 +0800

    add python template

--- flake.nix
+++ flake.nix
@@ -39,6 +39,10 @@
               path = ./templates/bevy;
               description = "Bevy template";
             };
+            python = {
+              path = ./templates/python;
+              description = "Python template";
+            };
           };
 
           nixosConfigurations = {
