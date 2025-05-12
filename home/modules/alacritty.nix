f267113360a9faa38aa61148e2e829442adfd7bf
dearfl <i@flr.me>
2025-05-12 13:56:44 +0800

    add comment for font setting

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -4,9 +4,8 @@ _: {
       enable = true;
       settings = {
         terminal.shell = "fish";
-
+        # this font have no korean or japanese variant?
         font.normal.family = "Fira Code";
-
       };
     };
   };
