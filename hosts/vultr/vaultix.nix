3953456814a970eeb42771253df7fe38af509cf3
dearfl <i@flr.me>
2025-05-12 21:46:39 +0800

    tmp host key

--- hosts/vultr/vaultix.nix
+++ hosts/vultr/vaultix.nix
@@ -2,7 +2,7 @@ _: {
   config = {
     vaultix = {
       settings = {
-        hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHIZCYb9mgVho/12X1GGlpU884ddWiysq3l3wl5qstCJ";
+        hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFGiEoanTL4qw9r875geTCmDd4N0FGlA25lmvlKwify";
       };
     };
   };
