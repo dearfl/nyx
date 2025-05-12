0fc8aadac70e74dd6a0910df0ea2edea0cb2e70f
dearfl <i@flr.me>
2025-05-12 13:55:57 +0800

    flatten alacritty font config

--- home/modules/alacritty.nix
+++ home/modules/alacritty.nix
@@ -4,9 +4,9 @@ _: {
       enable = true;
       settings = {
         terminal.shell = "fish";
-        font = {
-          normal = { family = "Fira Code"; };
-        };
+
+        font.normal.family = "Fira Code";
+
       };
     };
   };
