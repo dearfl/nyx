0234f035a5b7a635f371cfe481fdfbbdf025849b
dearfl <i@flr.me>
2025-05-18 03:20:07 +0800

    add disko setup for desktop

--- hosts/desktop/vaultix.nix
+++ hosts/desktop/vaultix.nix
@@ -1,5 +1,6 @@
 _: {
   config = {
+    # is nixos-anywhere --copy-host-keys working?
     vaultix = {
       settings = {
         hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeFC6b0yIdXpfSodZ+YFLaXDCFqP/1l3WkhJ768/TsU";
