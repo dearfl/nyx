280143ea94bbae531210cc094787c7caa0dfbedc
dearfl <i@flr.me>
2025-05-19 11:37:48 +0800

    add aliyun host

---
+++ hosts/aliyun/default.nix
@@ -0,0 +1,20 @@
+_: {
+  imports = [
+    ./disko.nix
+    ./hardware.nix
+    ./vaultix.nix
+
+    ../optional/grub.nix
+
+    ../optional/vaultix.nix
+    ../optional/v2server.nix
+
+    ../minimal
+  ];
+
+  time.timeZone = "Asia/Shanghai";
+  networking.hostName = "aliyun";
+
+  # state version, never change this
+  system.stateVersion = "25.05";
+}
---
+++ hosts/aliyun/disko.nix
@@ -0,0 +1,56 @@
+_: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            swap = {
+              size = "1024M";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                # randomEncryption = true;
+                # priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                # format = "ext4";
+                extraArgs = [ "-f" ]; # Override existing partition
+                mountpoint = "/";
+                mountOptions = [
+                  "compress=zstd"
+                  "noatime"
+                  # "defaults"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
