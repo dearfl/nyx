78ae00790fd6ba1a9249a97d87ba925adba3215f
dearfl <i@flr.me>
2025-05-23 11:22:28 +0800

    add disko setup for nyx

--- hosts/nyx/hardware.nix
+++ hosts/nyx/hardware.nix
@@ -24,27 +24,6 @@
     extraModulePackages = [ ];
   };
 
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/2326b934-c0f1-4566-8c9e-89cfa14006c3";
-      fsType = "btrfs";
-      options = [ "compress=zstd" ];
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/33F1-857F";
-      fsType = "vfat";
-      options = [
-        "fmask=0077"
-        "dmask=0077"
-      ];
-    };
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/f2527664-02e8-4665-9549-989f27f7249e"; }
-  ];
-
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
