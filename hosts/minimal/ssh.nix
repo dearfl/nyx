d2c345b106c1cdbffed0882f5a905220f2f7d695
dearfl <i@flr.me>
2025-05-28 16:29:50 +0800

    add sshfs

--- hosts/minimal/ssh.nix
+++ hosts/minimal/ssh.nix
@@ -3,6 +3,7 @@ _: {
     # let make is simple now
     services.openssh = {
       enable = true;
+      allowSFTP = true;
       settings = {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
