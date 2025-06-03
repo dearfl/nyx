3dec1dd80eacc88e043fa2fea912ea2e78d01dda
dearfl <i@flr.me>
2025-06-03 19:10:30 +0800

    update bevy templates

--- templates/bevy/flake.nix
+++ templates/bevy/flake.nix
@@ -1,46 +1,68 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       nixpkgs,
-      utils,
+      flake-utils,
+      rust-overlay,
       ...
     }:
-    utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        deps = with pkgs; [
-          pkg-config
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rustfmt"
+            "clippy"
+            "rust-analyzer"
+          ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        linker = pkgs.lib.optionals pkgs.stdenv.isLinux [
+          pkgs.clang
+          pkgs.mold
+        ];
+
+        nativeDeps = [ pkgs.pkg-config ];
+
+        linuxDeps = with pkgs; [
           udev
           alsa-lib
           vulkan-loader
           xorg.libX11
           xorg.libXcursor
           xorg.libXi
-          xorg.libXrandr
+          xorg.libXrandr # To use the x11 feature
           libxkbcommon
-          wayland
+          wayland # To use the wayland feature
         ];
+
+        darwinDeps = [ pkgs.apple-sdk_15 ];
+
+        deps =
+          (pkgs.lib.optionals pkgs.stdenv.isLinux linuxDeps)
+          ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin darwinDeps);
       in
       {
-        devShell =
-          with pkgs;
-          mkShell rec {
-            buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              clippy
-              rust-analyzer
-            ] ++ deps;
-            # xkbcommon use dlopen to load, so we need this envvar in dev shell
-            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-          };
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = nativeDeps ++ linker;
+          buildInputs = [ rust ] ++ deps;
+          # xkbcommon use dlopen to load, so we need this envvar in dev shell
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+        };
       }
     );
 }
