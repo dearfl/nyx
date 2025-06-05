5dc1783c63cff7e67c15eba2a1f6a62c25aa0a3a
dearfl <i@flr.me>
2025-06-05 10:29:58 +0800

    fix some issue caused by envvar differences

--- hosts/minimal/common.nix
+++ hosts/minimal/common.nix
@@ -1,5 +1,9 @@
 _: {
   config = {
     i18n.defaultLocale = "en_US.UTF-8";
+    # this should fix some issue caused by environment variable
+    # differences between system and user programs
+    # although we have other envvar to add like fcitx5-related ones
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
   };
 }
