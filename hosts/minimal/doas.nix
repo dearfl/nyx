dc92f7cee7d3f8ee64290177d41a44249db2b04a
dearfl <i@flr.me>
2025-06-25 17:12:20 +0800

    fix nix cache permission error

--- hosts/minimal/doas.nix
+++ hosts/minimal/doas.nix
@@ -4,7 +4,26 @@ let
     users = [ user ];
     keepEnv = true;
     persist = true;
-    setEnv = [ "SUDO_UID=${toString config.users.users.${user}.uid}" ];
+    setEnv = [
+      "SUDO_UID=${toString config.users.users.${user}.uid}"
+      "-HOME"
+      "-GTK_PATH"
+      "-INFOPATH"
+      "-LIBEXEC_PATH"
+      "-LOGNAME"
+      "-PWD"
+      "-NIX_USER_PROFILE_DIR"
+      "-NIX_XDG_DESKTOP_PORTAL_DIR"
+      "-QTWEBKIT_PLUGIN_PATH"
+      "-TERMINFO_DIRS"
+      "-USER"
+      "-XCURSOR_PATH"
+      "-XDG_CACHE_HOME"
+      "-XDG_CONFIG_DIRS"
+      "-XDG_CONFIG_HOME"
+      "-XDG_DATA_HOME"
+      "-XDG_STATE_HOME"
+    ];
   };
 in
 {
