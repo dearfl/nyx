665055279534c588541e76b2d3aa4c221d7b7cee
dearfl <i@flr.me>
2025-10-02 12:32:08 +0800

    add priority settings in wifi

--- hosts/optional/wifi.nix
+++ hosts/optional/wifi.nix
@@ -5,8 +5,14 @@ _: {
       enable = true;
       # hashed password, should be safe?
       networks = {
-        "glwifi-5g".pskRaw = "513ce170f82b9f9bba9a5a9ddc5db9a62a26903dc9eaed7e9059502c6169440a";
-        "WZKJ".pskRaw = "682fa633b86b1b6fadd82215a5a840cb55c85c59a9c4047d96408268adc31c31";
+        "glwifi-5g" = {
+          pskRaw = "513ce170f82b9f9bba9a5a9ddc5db9a62a26903dc9eaed7e9059502c6169440a";
+          priority = 1000;
+        };
+        "WZKJ" = {
+          pskRaw = "682fa633b86b1b6fadd82215a5a840cb55c85c59a9c4047d96408268adc31c31";
+          priority = 100;
+        };
       };
     };
   };
