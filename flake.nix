9468796b251deaa7b5630ae74ea5f0ae8dff5f5d
dearfl <i@flr.me>
2025-10-21 13:08:47 +0800

    add typst template

--- flake.nix
+++ flake.nix
@@ -54,6 +54,10 @@
               path = ./templates/cpp;
               description = "C++ template with CMake";
             };
+            typst = {
+              path = ./templates/typst;
+              description = "typst template";
+            };
           };
 
           nixosConfigurations = {
