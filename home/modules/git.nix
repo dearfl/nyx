cbd7845313785c5f32dc78c43e88bf1482ac68ec
dearfl <i@flr.me>
2025-10-23 18:54:43 +0800

    update git config

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -2,16 +2,18 @@ _: {
   config = {
     programs.git = {
       enable = true;
-      userName = "dearfl";
-      userEmail = "i@flr.me";
-      aliases = {
-        count-lines = "! git log --author=\"$1\" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\\n\", add, subs, loc }' #";
-        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      };
       difftastic = {
         enable = true;
       };
-      extraConfig = {
+      settings = {
+        user = {
+          name = "dearfl";
+          email = "i@flr.me";
+        };
+        aliases = {
+          count-lines = "! git log --author=\"$1\" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\\n\", add, subs, loc }' #";
+          lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        };
         init = {
           defaultBranch = "main";
         };
