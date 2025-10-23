275280e3e93789881d344c0dff0ab80e519ba7fa
dearfl <i@flr.me>
2025-10-23 20:30:04 +0800

    update git config

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -2,17 +2,20 @@ _: {
   config = {
     programs.git = {
       enable = true;
-      difftastic = {
-        enable = true;
-      };
       settings = {
         user = {
           name = "dearfl";
           email = "i@flr.me";
         };
-        aliases = {
+        alias = {
           count-lines = "! git log --author=\"$1\" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf \"added lines: %s, removed lines: %s, total lines: %s\\n\", add, subs, loc }' #";
           lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+          # `git log` with patches shown with difftastic.
+          dl = "-c diff.external=difft log -p --ext-diff";
+          # Show the most recent commit with difftastic.
+          ds = "-c diff.external=difft show --ext-diff";
+          # `git diff` with difftastic.
+          dft = "-c diff.external=difft diff";
         };
         init = {
           defaultBranch = "main";
