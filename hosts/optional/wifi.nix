0591bd024b995204652cd1c7f74df22eb5fcf411
dearfl <i@flr.me>
2025-10-28 19:08:29 +0800

    add cellphone hotspot setting

--- hosts/optional/wifi.nix
+++ hosts/optional/wifi.nix
@@ -3,8 +3,12 @@ _: {
     # wifi with wpa_supplicant
     networking.wireless = {
       enable = true;
+      # hashed password should be safe?
+      networks."dearfl" = {
+        pskRaw = "9b012899d5d7c6f3f43f85f3c26b4386b34231977f2fb21e0a5c096662b6831a";
+        priority = 10000;
+      };
       networks."glwifi-5g" = {
-        # hashed password, should be safe?
         pskRaw = "513ce170f82b9f9bba9a5a9ddc5db9a62a26903dc9eaed7e9059502c6169440a";
         priority = 1000;
       };
