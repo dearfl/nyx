9527cde5938685430a571bcbe974981fde3c2d32
dearfl <i@flr.me>
2025-11-09 15:57:24 +0800

    separate hypridle config

--- home/modules/wayland.nix
+++ home/modules/wayland.nix
@@ -3,37 +3,12 @@ _: {
     ./waybar.nix
     ./hyprland.nix
     ./hyprlock.nix
+    ./hypridle.nix
   ];
   config = {
     programs = {
       # we want some app launcher, default wofi is good enough
       wofi.enable = true;
-    };
-
-    services = {
-      # auto lock
-      hypridle = {
-        enable = true;
-        settings = {
-          general = {
-            after_sleep_cmd = "hyprctl dispatch dpms on";
-            ignore_dbus_inhibit = false;
-            lock_cmd = "hyprlock";
-          };
-
-          listener = [
-            {
-              timeout = 900;
-              on-timeout = "hyprlock";
-            }
-            {
-              timeout = 1200;
-              on-timeout = "hyprctl dispatch dpms off";
-              on-resume = "hyprctl dispatch dpms on";
-            }
-          ];
-        };
-      };
     };
   };
 }
